adb17b95417b0171c6e2d7f91d5a5a33e2ca6219
wldn143 <wldn143@gmail.com>
2022-11-28 23:37:13 +0900

    계정 생성

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,35 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      const existingUser = await client.user.findFirst({
+        where: { OR: [{ username }, { email }] },
+      });
+      if (!existingUser) {
+        return {
+          ok: false,
+          error: "This username/email is already taken.",
+        };
+      }
+      const uglyPassword = await bcrypt.hash(password, 10);
+      if (uglyPassword) {
+        client.user.create({
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            password: uglyPassword,
+          },
+        });
+        return {
+          ok: true,
+        };
+      }
+    },
+  },
+};
