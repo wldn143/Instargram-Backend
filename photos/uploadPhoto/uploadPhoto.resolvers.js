92e39dc6899c7c1a23ad12a07dec71ca260b1474
wldn143 <wldn143@gmail.com>
2022-12-29 22:12:10 +0900

    uploadPhoto할 때 caption에서 해시태그 분류해서 저장

---
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,36 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObjs = null;
+        if (caption) {
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObjs = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObjs.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObjs,
+              },
+            }),
+          },
+        });
+      }
+    ),
+  },
+};
