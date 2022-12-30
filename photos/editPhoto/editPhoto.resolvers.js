39a7bca9dff167f3efddbf4e13abe8e2676296c0
wldn143 <wldn143@gmail.com>
2022-12-30 21:55:53 +0900

    editPhoto 기능 추가

---
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -0,0 +1,46 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+import { processHashtags } from "../photos.utils";
+
+export default {
+  Mutation: {
+    editPhoto: protectedResolver(
+      async (_, { id, caption }, { loggedInUser }) => {
+        const oldPhotoHashtags = await client.photo
+          .findFirst({
+            where: { id, userId: loggedInUser.id },
+            //select: { hashtags: true },
+          })
+          .hashtags({
+            select: {
+              hashtag: true,
+            },
+          });
+
+        if (!oldPhotoHashtags) {
+          return {
+            ok: false,
+            error: "Photo not found.",
+          };
+        }
+        const photo = await client.photo.update({
+          where: {
+            id,
+          },
+          data: {
+            caption,
+            hashtags: {
+              disconnect: oldPhotoHashtags,
+              connectOrCreate: processHashtags(caption),
+            },
+          },
+        });
+        // if (photo) {
+        //   return {
+        //     ok: true,
+        //   };
+        // }
+      }
+    ),
+  },
+};
