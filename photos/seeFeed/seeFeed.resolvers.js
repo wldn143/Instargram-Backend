5b9e84f65d7f48b01af8e9865d9899c9c4fbe1d7
wldn143 <wldn143@gmail.com>
2022-12-31 14:56:51 +0900

    seeFeed 기능 추가

---
+++ photos/seeFeed/seeFeed.resolvers.js
@@ -0,0 +1,20 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver(async (_, { offset }, { loggedInUser }) =>
+      client.photo.findMany({
+        where: {
+          OR: [
+            { user: { followers: { some: { id: loggedInUser.id } } } },
+            { userId: loggedInUser.id },
+          ],
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    ),
+  },
+};
