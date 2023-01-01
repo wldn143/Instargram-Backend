6923b5fe300893e3d5df80bedf11648706e558d1
wldn143 <wldn143@gmail.com>
2023-01-01 21:44:09 +0900

    S3 Photo Upload

---
+++ shared/shared.utils.js
@@ -0,0 +1,20 @@
+import AWS from "aws-sdk";
+export const uploadToS3 = async (file, userId, folderName) => {
+  AWS.config.update({
+    credentials: {
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
+    },
+  });
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: "insta-clone-uploads",
+      Key: objectName,
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
 import { createWriteStream } from "fs";
+import { uploadToS3 } from "../../shared/shared.utils";
 const resolverFn = async (
   _,
   { firstName, lastName, username, email, password: newPassword, bio, avatar },
@@ -9,14 +10,15 @@ const resolverFn = async (
 ) => {
   let avatarUrl = null;
   if (avatar) {
-    const { filename, createReadStream } = await avatar;
-    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
-    const readStream = createReadStream();
-    const writeStream = createWriteStream(
-      process.cwd() + "/uploads/" + newFilename
-    );
-    readStream.pipe(writeStream);
-    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+    avatarUrl = await uploadToS3(avatar, loggedInUser.id, "avatars");
+    // const { filename, createReadStream } = await avatar;
+    // const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+    // const readStream = createReadStream();
+    // const writeStream = createWriteStream(
+    //   process.cwd() + "/uploads/" + newFilename
+    // );
+    // readStream.pipe(writeStream);
+    // avatarUrl = `http://localhost:4000/static/${newFilename}`;
   }
   let uglyPassword = null;
   if (newPassword) {
