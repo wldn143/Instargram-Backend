d8143b22396cf97d52eedf6305f3a8f322753053
wldn143 <wldn143@gmail.com>
2023-01-04 16:20:24 +0900

    subscription SetUp

--- messages/sendMessage/sendMessage.resolvers.js
+++ messages/sendMessage/sendMessage.resolvers.js
@@ -1,4 +1,6 @@
 import client from "../../client";
+import { NEW_MESSAGE } from "../../constants";
+import pubsub from "../../pubsub";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
@@ -54,7 +56,7 @@ export default {
           }
         }
 
-        await client.message.create({
+        const message = await client.message.create({
           data: {
             payload,
             user: {
@@ -65,6 +67,7 @@ export default {
             },
           },
         });
+        pubsub.publish(NEW_MESSAGE, { roomUpdates: { ...message } });
         return {
           ok: true,
         };
--- server.js
+++ server.js
@@ -2,23 +2,30 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import logger from "morgan";
+import http from "http";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
+    if (req) {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    }
   },
 });
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(logger("tiny"));
-server.applyMiddleware({ app });
+apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: PORT }, () => {
   console.log(`🚀Server is running on http://localhost:${PORT}/graphql  ✅`);
 });
